ee8308bf82e05f17055fbab733ff128de97d31e5
yujinlee <now8567@hanmail.net>
2023-09-10 11:59:30 +0900

    Section4. 통계_어플리케이션 단 코드로 처리

--- src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
+++ src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
@@ -8,6 +8,7 @@ import com.group.libraryapp.domain.user.loanhistory.UserLoanStatus
 import com.group.libraryapp.dto.book.request.BookLoanRequest
 import com.group.libraryapp.dto.book.request.BookRequest
 import com.group.libraryapp.dto.book.request.BookReturnRequest
+import com.group.libraryapp.dto.book.response.BookStatResponse
 import com.group.libraryapp.util.fail
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -38,5 +39,29 @@ class BookService (
     fun returnBook(request: BookReturnRequest){
         val user = userRepository.findByName(request.userName) ?: fail()
         user.returnBook(request.bookName)
+    }
+
+    @Transactional(readOnly = true)
+    fun countLoanedBook(): Int {
+        return userLoanHistoryRepository.findAllByStatus(UserLoanStatus.LOANED).size
+    }
+
+    @Transactional(readOnly = true)
+    fun getBookStatistics(): List<BookStatResponse> {
+
+        return bookRepository.findAll() // List<Book>
+            .groupBy { book -> book.type } // Map<BookType, List<Book>>
+            .map { (type, books) -> BookStatResponse(type, books.size) }
+
+
+     /*   val results = mutableListOf<BookStatResponse>()
+        val books = bookRepository.findAll()
+        for (book in books) {
+            results.firstOrNull{ dto -> book.type == dto.type }
+                ?.plusOne()
+                ?: results.add(BookStatResponse(book.type, 1))
+        return results
+
+        }*/
     }
 }
